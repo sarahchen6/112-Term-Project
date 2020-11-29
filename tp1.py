22ff0b0901d694ea66e74177fe7adf338fd9d1df
Sarah Chen <sarah.chen6@gmail.com>
2020-11-28 23:33:19 -0500

    Update tp1.py

--- tp1.py
+++ tp1.py
@@ -135,18 +135,18 @@ class AvatarScreen(StoryScreen):
             and (50 <= event.y <= 100)):
             mode.nextButtonColor = 'white'
             mode.nextButtonTextColor = 'sea green'
-        elif ((mode.width/2-100 <= event.x <= mode.width/2+100) 
+        elif (((event.x < 50) or ((event.x > 150) 
+            and (event.x < mode.width-150)) or (event.x > mode.width-50)
+            or ((event.y < 50) or (event.y > 100)))):
+            mode.backButtonColor = mode.nextButtonColor = 'sea green'
+            mode.backButtonTextColor = mode.nextButtonTextColor = 'white'
+        if ((mode.width/2-100 <= event.x <= mode.width/2+100) 
             and (mode.height*9/10-50 <= event.y <= mode.height*9/10+50)):
             mode.startButtonColor = 'white'
             mode.startButtonTextColor = 'sea green'
-        elif (((event.x < 50) or ((event.x > 150) 
-            and (event.x < mode.width-150)) or (event.x > mode.width-50)
-            or ((event.y < 50) or (event.y > 100)))
-            or ((event.x < mode.width/2-100) or (event.x > mode.width/2+100)
+        elif ((event.x < mode.width/2-100) or (event.x > mode.width/2+100)
             or (event.y < mode.height*9/10-50) 
-            or (event.y < mode.height*9/10+50))):
-            mode.backButtonColor = mode.nextButtonColor = 'sea green'
-            mode.backButtonTextColor = mode.nextButtonTextColor = 'white'
+            or (event.y < mode.height*9/10+50)):
             mode.startButtonColor = 'sea green'
             mode.startButtonTextColor = 'white'
         # add info button toggling after location is set?
@@ -266,16 +266,16 @@ class FinalScreen(GameModeScreen):
             and (mode.height*9/10-25 <= event.y <= mode.height*9/10+25)):
             mode.homeButtonColor = 'black'
             mode.homeButtonTextColor = 'gray'
-        elif ((event.x < mode.width/2-200) or (event.x > mode.width/2+200)
-            or (event.y < mode.height*7/10-50) 
-            or (event.y > mode.height*7/10+50)):
-            mode.playAgainButtonColor = 'sea green'
-            mode.playAgainButtonTextColor = 'white'
         elif ((event.x < mode.width/2-50) or (event.x > mode.width/2+50)
             or (event.y < mode.height*9/10-25) 
             or (event.y > mode.height*9/10+25)):
             mode.homeButtonColor = 'gray'
             mode.homeButtonTextColor = 'black'
+        if ((event.x < mode.width/2-200) or (event.x > mode.width/2+200)
+            or (event.y < mode.height*7/10-50) 
+            or (event.y > mode.height*7/10+50)):
+            mode.playAgainButtonColor = 'sea green'
+            mode.playAgainButtonTextColor = 'white'
 
     def mousePressed(mode, event):
         if ((mode.width/2-200 <= event.x <= mode.width/2+200) 
@@ -317,6 +317,90 @@ class FinalScreen(GameModeScreen):
                             font='PlayfairDisplay 20 bold')
 
 MyApp(width=1000, height=800)
+
+
+
+########################################
+#   Santa Maze Game
+#   Sarah Chen (sarahc2)
+########################################
+#   Buttons
+########################################
+
+class Buttons(object):
+    def __init__(self, label):
+        self.label = label
+
+    def startButton(self, canvas):
+        canvas.create_rectangle(self.width/2-150, self.height*2/3-50,
+                            self.width/2+150, self.height*2/3+50,
+                            fill='dark sea green')
+        canvas.create_text(self.width/2, self.height*2/3,
+                            fill='white',
+                            text=self.label, 
+                            font='PlayfairDisplay 40 bold')
+        
+        
+        
+########################################
+#   Maze Game
+#   Coded by: Sarah Chen (sarahc2)
+########################################
+#
+#   Citations: 
+#   1)  http://weblog.jamisbuck.org/2011/1/12/
+#       maze-generation-recursive-division-algorithm
+#
+########################################
+from cmu_112_graphics import *
+import random
+
+# experimenting with drawing random lines one at a time
+def chooseOrientation(width, height):
+    if (width < height):
+        return 'horizontal'
+    elif (height > width):
+        return 'vertical'
+    else:
+        return random.choice(['horizontal', 'vertical'])
+
+def appStarted(app):
+    app.yValues = list()
+    app.xValues = list()
+    app.dimension = 5
+
+def defineYValues(app, width, height, dimension):
+    orientation = chooseOrientation(width, height)
+    if (orientation == 'horizontal') or (orientation == 'vertical'):
+        yValue = random.randint(1, app.dimension)
+        app.yValues.append(yValue)
+    print('yValues: ', app.yValues)
+
+def defineXValues(app, width, height, dimension):
+    orientation = chooseOrientation(width, height)
+    if (orientation == 'horizontal') or (orientation == 'vertical'):
+        xValue = random.randint(1, app.dimension)
+        app.xValues.append(xValue)
+    print('xValues: ', app.xValues)
+
+def keyPressed(app, event):
+    if (event.key == 'y'):
+        defineYValues(app, app.width, app.height, app.dimension)
+    elif (event.key == 'x'):
+        defineXValues(app, app.width, app.height, app.dimension)
+
+def drawMaze(app, canvas):
+    for yValue in app.yValues:
+        yValue = yValue*(app.height/app.dimension)
+        canvas.create_line(0, yValue, app.width, yValue)
+    for xValue in app.xValues:
+        xValue = xValue*(app.width/app.dimension)
+        canvas.create_line(xValue, 0, xValue, app.height)
+
+def redrawAll(app, canvas):
+    drawMaze(app, canvas)
+
+runApp(width=400, height=400)
 
 
 
@@ -420,7 +504,7 @@ def getMazeSolutionConnections(n):
         for point in mazeDict[key]:
             connDict[point].append((key))
 
-    print(f'Dict: {mazeDict} \n Sol: {solution} \n Conn: {connDict}')
+    # print(f'Dict: {mazeDict} \n Sol: {solution} \n Conn: {connDict}')
     return mazeDict, solution, connDict
 
 # getMazeSolutionConnections(2)
@@ -437,30 +521,114 @@ from mazeGenerationAndSolution import *
 from cmu_112_graphics import *
 
 def appStarted(app):
-    app.n = 15
+    app.n = 35 # 38+ has MVC Violation
     app.cellWidth = app.width / app.n
     app.cellHeight = app.height / app.n
     app.lineWidth = app.height / (app.n * 10)
     app.lineMargin = app.lineWidth * 2 / 5
-    app.lines = set()
     app.dotX = app.cellWidth / 2
     app.dotY = app.cellHeight / 2
     app.dotR = min(app.cellWidth, app.cellHeight) / 3
+    app.dotStepSize = app.dotR / 2
     app.mazeDict, app.solution, app.connDict = getMazeSolutionConnections(app.n)
     app.showSolution = False
 
 def keyPressed(app, event):
     if (event.key == 'Space'):
         app.showSolution = not app.showSolution
+    elif (event.key == 'r'):
+        appStarted(app)
     elif (event.key == 'Up'):
-        app.dotY -= 10
+        possibleMoves = getPossibleMoves(app)
+        print(possibleMoves)
+        if (app.dotY > app.dotStepSize) and ('Up' in possibleMoves):
+            app.dotY -= app.dotStepSize
     elif (event.key == 'Down'):
-        app.dotY += 10
+        possibleMoves = getPossibleMoves(app)
+        print(possibleMoves)
+        if ((app.dotY < app.height - app.dotStepSize) 
+            and ('Down' in possibleMoves)):
+            app.dotY += app.dotStepSize
     elif (event.key == 'Left'):
-        app.dotX -= 10
+        possibleMoves = getPossibleMoves(app)
+        print(possibleMoves)
+        if (app.dotX > app.dotStepSize) and ('Left' in possibleMoves):
+            app.dotX -= app.dotStepSize
     elif (event.key == 'Right'):
-        app.dotX += 10
+        possibleMoves = getPossibleMoves(app)
+        print(possibleMoves)
+        if ((app.dotX < app.width - app.dotStepSize) 
+            and ('Right' in possibleMoves)):
+            app.dotX += app.dotStepSize
  
+def getPossibleMoves(app):
+    # center and radius of dot
+    cx, cy, r = app.dotX, app.dotY, app.dotR
+    # bounds of cell that center of dot is in
+    centerx0, centerx1, centery0, centery1 = getCellBounds(app,centerX,centerY)
+    # left, right, top, and bottom of dot
+    dotx0, dotx1, doty0, doty1 = cx-r, cx+r, cy-r, cy+r
+    midX = (dotx0 + dotx1) / 2
+    midY = (doty0 + doty1) / 2
+
+    # cell that center, top, bottom, left, and right of dot is in
+    centerX, centerY = getCell(app, cx, cy)
+    cellOfCenter = (centerX, centerY)
+    aboveX, aboveY = getCell(app, midX, doty0)
+    cellOfTop = (aboveX, aboveY)
+    belowX, belowY = getCell(app, midX, doty1)
+    cellOfBottom = (belowX, belowY)
+    leftX, leftY = getCell(app, dotx0, midY)
+    cellOfLeft = (leftX, leftY)
+    rightX, rightY = getCell(app, dotx1, midY)
+    cellOfRight = (rightX, rightY)
+
+    possibleMoves = set()
+
+    # check if dot can move within cell
+    if (doty0 - app.dotStepSize > centery0 + (app.lineWidth * 2 / 3)):
+        possibleMoves.add('Up')
+    if (doty1 + app.dotStepSize < centery1 - (app.lineWidth * 2 / 3)):
+        possibleMoves.add('Down')
+    if (dotx0 - app.dotStepSize > centerx0 + (app.lineWidth * 2 / 3)):
+        possibleMoves.add('Left')
+    if (dotx1 + app.dotStepSize < centerx1 - (app.lineWidth * 2 / 3)):
+        possibleMoves.add('Right')
+
+    # cells above, below, left, and right of center cell
+    cellAbove = (centerX, centerY-1)
+    cellBelow = (centerX, centerY+1)
+    cellLeft = (centerX-1, centerY)
+    cellRight = (centerX+1, centerY)
+
+    # check if dot can move to next cell
+    if ((cellAbove in app.connDict[cellOfLeft]) 
+        and (cellAbove in app.connDict[cellOfRight])):
+        possibleMoves.add('Up')
+    if ((cellBelow in app.connDict[cellOfLeft]) 
+        and (cellBelow in app.connDict[cellOfRight])):
+        possibleMoves.add('Down')
+    if ((cellLeft in app.connDict[cellOfTop]) 
+        and (cellLeft in app.connDict[cellOfBottom])):
+        possibleMoves.add('Left')
+    if ((cellRight in app.connDict[cellOfTop]) 
+        and (cellRight in app.connDict[cellOfBottom])):
+        possibleMoves.add('Right')
+
+    return possibleMoves
+
+def getCell(app, cx, cy):
+    cellX = cx // app.cellWidth
+    cellY = cy // app.cellHeight
+    return int(cellX), int(cellY)
+
+def getCellBounds(app, x, y):
+    x0 = x * app.cellWidth
+    x1 = x * app.cellWidth + app.cellWidth
+    y0 = y * app.cellHeight
+    y1 = y * app.cellHeight + app.cellHeight
+    return x0, x1, y0, y1
+
 def redrawAll(app, canvas):
     if (app.showSolution == True):
         for point in app.solution:
@@ -482,8 +650,7 @@ def redrawAll(app, canvas):
             drawRightLine(app, canvas, point)
 
     canvas.create_oval(app.dotX - app.dotR, app.dotY - app.dotR,
-                    app.dotX + app.dotR, app.dotY + app.dotR,
-                    fill='red')
+                    app.dotX + app.dotR, app.dotY + app.dotR, fill='red')
 
 def drawAboveLine(app, canvas, point):
     x, y = point
