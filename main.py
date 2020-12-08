e011ad5e17205739799f17b02fbd05a6ec74410b
Sarah Chen <sarah.chen6@gmail.com>
2020-12-08 02:35:11 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+########################################
+#   Santa Maze Game (main.py)
+#   By: Sarah Chen (sarahc2)
+########################################
+#   Combine gameScreens and mazeModes
+########################################
+#
+#   Citations: 
+#   1)  incorporated subclassing ModalApp and Mode idea from
+#       https://www.cs.cmu.edu/~112/notes/notes-animations-part3.html
+#
+########################################
+from gameScreens import *
+from mazeModes import *
+
+class MyApp(ModalApp):
+    def appStarted(app):
+        # from gameScreens
+        app.titleScreen = TitleScreen()
+        app.backgroundScreen = BackgroundScreen()
+        app.instructionsScreen = InstructionsScreen()
+        app.sleighScreen = SleighScreen()
+        app.finalScreen = FinalScreen()
+
+        # from mazeModes
+        app.maze = Maze()
+        app.radiusMode = RadiusMode()
+        app.grinchMode = GrinchMode()
+        app.presents = 100
+        app.finalPresents = 100
+        app.timeSec = 0
+        app.timeMin = 0
+
+        app.timerMode = True
+        app.timerDelay = 10
+
+        app.setActiveMode(app.finalScreen)
+
+    def timerFired(app):
+        app._activeMode.timerFired()
+
+
+
+MyApp(width=1000, height=800)
